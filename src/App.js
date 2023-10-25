d20ec3436e39cf0cf941cda152683ebb937f3b78
im-TahiraBatool <138373725+im-TahiraBatool@users.noreply.github.com>
2023-10-26 00:32:41 +0500

    commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import ProductList from "./Components/ProductList.js";
+import React, { useState } from "react";
+import Footer from "./Components/Footer";
+import AddItem from "./Components/AddItem";
+
+function App() {
+  const products = [
+    {
+      price: 99999,
+      name: "IPhone 10S MAX",
+      quantity: 0,
+    },
+    {
+      price: 28999,
+      name: "Redmi Note 10S MAX",
+      quantity: 0,
+    },
+  ];
+  let [productList, setProductList] = useState(products);
+  let [totalAmount, setTotalAmount] = useState(0);
+
+  const incrementQuantity = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+    newProductList[index].quantity++;
+    newTotalAmount += newProductList[index].price;
+    setTotalAmount(newTotalAmount);
+    setProductList(newProductList);
+  };
+  const decrementQuantity = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+    if (newProductList[index].quantity > 0) {
+      newProductList[index].quantity--;
+      newTotalAmount -= newProductList[index].price;
+    }
+    setTotalAmount(newTotalAmount);
+    setProductList(newProductList);
+  };
+
+  const resetQuantity = () => {
+    let newProductList = [...productList];
+    newProductList.map((products) => {
+      products.quantity = 0;
+    });
+    setProductList(newProductList);
+    setTotalAmount(0);
+  };
+  const removeItem = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+    newTotalAmount -=
+      newProductList[index].quantity * newProductList[index].price;
+    newProductList.splice(index, 1);
+    setProductList(newProductList);
+    setTotalAmount(newTotalAmount);
+  };
+
+  const addItem = (name, price) => {
+    let newProductList = [...productList];
+    newProductList.push({
+      price: price,
+      name: name,
+      quantity: 0,
+    });
+    setProductList(newProductList);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <main className="container mt-5">
+        <AddItem addItem={addItem} />
+        <ProductList
+          productList={productList}
+          incrementQuantity={incrementQuantity}
+          decrementQuantity={decrementQuantity}
+          removeItem={removeItem}
+        />
+      </main>
+      {<Footer totalAmount={totalAmount} resetQuantity={resetQuantity} />}
+    </>
+  );
+}
+
+export default App;
